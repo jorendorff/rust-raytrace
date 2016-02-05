202ebd2979015277ffb22c724ba153a14ae158b6
Jason Orendorff <jason.orendorff@gmail.com>
2016-02-05 08:31:53 -0600

    normals

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,47 @@ mod vec;
 use vec::{Vec3, Ray};
 
 
-fn hit_sphere(center: Vec3, radius: f32, r: Ray) -> bool {
+fn hit_sphere(center: Vec3, radius: f32, r: Ray) -> Option<Vec3> {
     let oc = r.origin() - center;
     let a = r.direction().dot(r.direction());
     let b = 2.0 * oc.dot(r.direction());
     let c = oc.dot(oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant > 0.0 {
+        let d = 1.0 / (2.0 * a);
+        // Assuming r.origin() is outside the sphere, we only need to consider
+        // this hit and not the other hit (with the positive square root)
+        let t = (-b - discriminant.sqrt()) * d;
+        if t <= 0.0 {
+            None
+        } else {
+            let p = r.point_at_parameter(t);
+            Some(p - center)
+        }
+    } else {
+        None
+    }
 }
 
 fn color(r: Ray) -> Vec3 {
     const WHITE: Vec3 = Vec3(1.0, 1.0, 1.0);
     const SKY_BLUE: Vec3 = Vec3(0.5, 0.7, 1.0);
     const RED: Vec3 = Vec3(1.0, 0.0, 0.0);
+    const GLANCING: Vec3 = Vec3(0.3, 0.0, 0.5);
 
-    if hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, r) {
-        return RED;
-    }
     let unit_direction = r.direction().to_unit_vector();
+    if let Some(n) = hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, r) {
+        let d = n.to_unit_vector().dot(unit_direction).abs();  // directness
+        return (1.0 - d) * GLANCING + d * RED;
+    }
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * WHITE + t * SKY_BLUE
 }
 
 
 fn main() {
-    const WIDTH: usize = 200;
-    const HEIGHT: usize = 100;
+    const WIDTH: usize = 400;
+    const HEIGHT: usize = 300;
 
     println!("P3");
     println!("{} {}", WIDTH, HEIGHT);
@@ -38,9 +53,9 @@ fn main() {
     let origin = Vec3(0.0, 0.0, 0.0);
 
     // direction vectors defining the view
-    let lower_left_corner = Vec3(-2.0, -1.0, -1.0);
+    let lower_left_corner = Vec3(-2.0, -1.5, -1.0);
     let horizontal = Vec3(4.0, 0.0, 0.0);
-    let vertical = Vec3(0.0, 2.0, 0.0);
+    let vertical = Vec3(0.0, 3.0, 0.0);
 
     for y in 0 .. HEIGHT {
         let j = HEIGHT - 1 - y;
