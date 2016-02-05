f290cb58ccbefb1a3c91c22f799929e255a87e3d
Jason Orendorff <jason.orendorff@gmail.com>
2016-02-05 08:13:36 -0600

    sphere

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
 mod vec;
 
-use vec::Vec3;
+use vec::{Vec3, Ray};
+
+
+fn hit_sphere(center: Vec3, radius: f32, r: Ray) -> bool {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = 2.0 * oc.dot(r.direction());
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant > 0.0
+}
+
+fn color(r: Ray) -> Vec3 {
+    const WHITE: Vec3 = Vec3(1.0, 1.0, 1.0);
+    const SKY_BLUE: Vec3 = Vec3(0.5, 0.7, 1.0);
+    const RED: Vec3 = Vec3(1.0, 0.0, 0.0);
+
+    if hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, r) {
+        return RED;
+    }
+    let unit_direction = r.direction().to_unit_vector();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * WHITE + t * SKY_BLUE
+}
+
 
 fn main() {
     const WIDTH: usize = 200;
@@ -9,13 +33,22 @@ fn main() {
     println!("P3");
     println!("{} {}", WIDTH, HEIGHT);
     println!("255");
+
+    // camera position (a point)
+    let origin = Vec3(0.0, 0.0, 0.0);
+
+    // direction vectors defining the view
+    let lower_left_corner = Vec3(-2.0, -1.0, -1.0);
+    let horizontal = Vec3(4.0, 0.0, 0.0);
+    let vertical = Vec3(0.0, 2.0, 0.0);
+
     for y in 0 .. HEIGHT {
         let j = HEIGHT - 1 - y;
         for i in 0 .. WIDTH {
-            let rgb = Vec3 (
-                i as f32 / WIDTH as f32,
-                j as f32 / HEIGHT as f32,
-                0.2).to_u8();
+            let u = i as f32 / WIDTH as f32;
+            let v = j as f32 / HEIGHT as f32;
+            let r = Ray(origin, lower_left_corner + u * horizontal + v * vertical);
+            let rgb = color(r).to_u8();
             println!("{} {} {}", rgb[0], rgb[1], rgb[2]);
         }
     }
--- src/vec.rs
+++ src/vec.rs
@@ -1,6 +1,6 @@
 use std::ops::*;
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Clone, Copy, Debug)]
 pub struct Vec3(pub f32, pub f32, pub f32);
 
 impl Vec3 {
@@ -19,10 +19,15 @@ impl Vec3 {
     }
 
     pub fn squared_length(self) -> f32 { self.dot(self) }
+    pub fn length(self) -> f32 { self.squared_length().sqrt() }
 
     pub fn to_u8(&self) -> [u8;3] {
         let u = |f| (f * 255.9) as i32 as u8;
         [u(self.0), u(self.1), u(self.2)]
+    }
+
+    pub fn to_unit_vector(&self) -> Vec3 {
+        *self / self.length()
     }
 }
 
@@ -45,4 +50,28 @@ impl Sub for Vec3 {
     fn sub(self, other: Vec3) -> Vec3 {
         Vec3(self.0 - other.0, self.1 - other.1, self.2 - other.2)
     }
+}
+
+impl Mul<Vec3> for f32 {
+    type Output = Vec3;
+    fn mul(self, v: Vec3) -> Vec3 {
+        Vec3(self * v.0, self * v.1, self * v.2)
+    }
+}
+
+impl Div<f32> for Vec3 {
+    type Output = Vec3;
+    fn div(self, r: f32) -> Vec3 {
+        (1.0 / r) * self
+    }
+}
+
+
+#[derive(Clone, Copy, Debug)]
+pub struct Ray(pub Vec3, pub Vec3);
+
+impl Ray {
+    pub fn origin(&self) -> Vec3 { self.0 }
+    pub fn direction(&self) -> Vec3 { self.1 }
+    pub fn point_at_parameter(&self, t: f32) -> Vec3 { self.0 + t * self.1 }
 }
