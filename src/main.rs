137e0d8dd3b840112893e7cf4aa48c39c4b1a281
Jason Orendorff <jason.orendorff@gmail.com>
2016-02-05 08:38:06 -0600

    HitTest trait.

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,50 @@ mod vec;
 
 use vec::{Vec3, Ray};
 
+struct Hit {
+    t: f32,
+    p: Vec3,
+    normal: Vec3
+}
 
-fn hit_sphere(center: Vec3, radius: f32, r: Ray) -> Option<Vec3> {
-    let oc = r.origin() - center;
-    let a = r.direction().dot(r.direction());
-    let b = 2.0 * oc.dot(r.direction());
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
-    if discriminant > 0.0 {
-        let d = 1.0 / (2.0 * a);
-        // Assuming r.origin() is outside the sphere, we only need to consider
-        // this hit and not the other hit (with the positive square root)
-        let t = (-b - discriminant.sqrt()) * d;
-        if t <= 0.0 {
-            None
+trait HitTest {
+    fn hit(&self, r: &Ray) -> Option<Hit>;
+}
+
+struct Sphere {
+    center: Vec3,
+    radius: f32
+}
+
+impl HitTest for Sphere {
+    fn hit(&self, r: &Ray) -> Option<Hit> {
+        let oc = r.origin() - self.center;
+        let a = r.direction().dot(r.direction());
+        let b = 2.0 * oc.dot(r.direction());
+        let c = oc.dot(oc) - self.radius * self.radius;
+        let discriminant = b * b - 4.0 * a * c;
+        if discriminant > 0.0 {
+            let d = 1.0 / (2.0 * a);
+            // Assuming r.origin() is outside the sphere, we only need to consider
+            // this hit and not the other hit (with the positive square root)
+            let t = (-b - discriminant.sqrt()) * d;
+            if t <= 0.0 {
+                None
+            } else {
+                let p = r.point_at_parameter(t);
+                Some(Hit {
+                    t: t,
+                    p: p,
+                    normal: p - self.center
+                })
+            }
         } else {
-            let p = r.point_at_parameter(t);
-            Some(p - center)
+            None
         }
-    } else {
-        None
     }
 }
+
+
 
 fn color(r: Ray) -> Vec3 {
     const WHITE: Vec3 = Vec3(1.0, 1.0, 1.0);
@@ -31,9 +53,14 @@ fn color(r: Ray) -> Vec3 {
     const RED: Vec3 = Vec3(1.0, 0.0, 0.0);
     const GLANCING: Vec3 = Vec3(0.3, 0.0, 0.5);
 
+    let sphere = Sphere {
+        center: Vec3(0.0, 0.0, -1.0),
+        radius: 0.8
+    };
+
     let unit_direction = r.direction().to_unit_vector();
-    if let Some(n) = hit_sphere(Vec3(0.0, 0.0, -1.0), 0.5, r) {
-        let d = n.to_unit_vector().dot(unit_direction).abs();  // directness
+    if let Some(hit) = sphere.hit(&r) {
+        let d = hit.normal.to_unit_vector().dot(unit_direction).abs();  // directness
         return (1.0 - d) * GLANCING + d * RED;
     }
     let t = 0.5 * (unit_direction.y() + 1.0);
