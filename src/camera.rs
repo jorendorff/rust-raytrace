fc146480ef43f1fd60b0ad8ba512c2ac8ad3af5d
Jason Orendorff <jason.orendorff@gmail.com>
2016-03-19 19:09:04 -0500

    Add vertical field of view as an option when creating a `Camera`.

--- src/camera.rs
+++ src/camera.rs
@@ -1,5 +1,7 @@
 use vec::{Vec3, Ray};
 
+use std::f32::consts::PI;
+
 pub struct Camera {
     origin: Vec3,
     lower_left_corner: Vec3,
@@ -8,12 +10,15 @@ pub struct Camera {
 }
 
 impl Camera {
-    pub fn new() -> Camera {
+    pub fn new(vfov_degrees: f32, aspect: f32) -> Camera {
+        let theta = vfov_degrees * PI / 180.0;
+        let half_height = (theta / 2.0).tan();
+        let half_width = aspect * half_height;
         Camera {
-            origin: Vec3(0.0, 0.0, 1.0),
-            vertical: Vec3(0.0, 2.0, 0.0),
-            horizontal: Vec3(4.0, 0.0, 0.0),
-            lower_left_corner: Vec3(-2.0, -1.0, -1.0)
+            origin: Vec3(0.0, 0.0, 0.0),
+            vertical: Vec3(0.0, 2.0 * half_height, 0.0),
+            horizontal: Vec3(2.0 * half_width, 0.0, 0.0),
+            lower_left_corner: Vec3(-half_width, -half_height, -1.0)
         }
     }
 
